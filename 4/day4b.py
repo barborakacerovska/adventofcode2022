91649590d5563ef8f4854779d080af6350e334c8
barbora.kacerovska <barbora.kacerovska@bizztreat.com>
2022-12-04 18:16:25 +0100

    day4

---
+++ 4/day4b.py
@@ -0,0 +1,27 @@
+
+def get_ordered(pair):
+    """
+    order by the first number
+    """
+    if get_number(pair,0,0) > get_number(pair,1,0):
+        return pair[::-1]
+    else:
+        return pair
+
+def get_number(pair,a,b):
+    return int(pair[a].split('-')[b])
+
+def compare_last_and_first(pair):
+    """
+    compare the last numbers of the pair
+    """
+    if int(pair[0].split('-')[1]) >= int(pair[1].split('-')[0]):
+        return 1
+    else:
+        return 0
+
+with open('input.txt') as t:
+   common_values = ([compare_last_and_first(get_ordered(x.strip().split(','))) for x in t.readlines()])
+    
+
+print(sum(common_values))
